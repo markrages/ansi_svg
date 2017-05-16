5c2fd552ef9f53f9b2005cd87ea0dd4329e38beb
Mark Rages <mrages@sram.com>
2017-05-16 17:59:06 -0600

    Ignore unknown codes.

--- ansi_enscript.py
+++ ansi_enscript.py
@@ -1,33 +1,58 @@
 #!/usr/bin/python2
 
 import re
+import sys
 
-def rgb(r,g,b): return "\0color{%f %f %f}"%(r,g,b)
+def fg_rgb(r,g,b): return "\0color{%f %f %f}"%(r/255.,g/255.,b/255.)
+def bg_rgb(r,g,b): return "\0bgcolor{%f %f %f}"%(r/255.,g/255.,b/255.)
 
 # These are PuTTY colors.
 # From https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
 
 br_on = 255
 br_off = 85
-norm_on = 187
 
-colortab = {(0,30):rgb( br_off, br_off, br_off), # black
-            (0,31):rgb(  br_on, br_off, br_off), # red
-            (0,32):rgb( br_off,  br_on, br_off), # green
-            (0,33):rgb(  br_on,  br_on, br_off), # yellow
-            (0,34):rgb( br_off, br_off,  br_on), # blue
-            (0,35):rgb(  br_on, br_off,  br_on), # magenta
-            (0,36):rgb( br_off,  br_on,  br_on), # cyan
-            (0,37):rgb(  br_on,  br_on,  br_on), # white
-            (1,30):rgb(      0,      0,      0), # black
-            (1,31):rgb(norm_on,      0,      0), # red
-            (1,32):rgb(      0,norm_on,      0), # green
-            (1,33):rgb(norm_on,norm_on,      0), # yellow
-            (1,34):rgb(      0,      0,norm_on), # blue
-            (1,35):rgb(norm_on,      0,norm_on), # magenta
-            (1,36):rgb(      0,norm_on,norm_on), # cyan
-            (1,37):rgb(norm_on,norm_on,norm_on)  # white
-            }
+boldtab = {
+    30:fg_rgb( br_off, br_off, br_off), # black
+    31:fg_rgb(  br_on, br_off, br_off), # red
+    32:fg_rgb( br_off,  br_on, br_off), # green
+    33:fg_rgb(  br_on,  br_on, br_off), # yellow
+    34:fg_rgb( br_off, br_off,  br_on), # blue
+    35:fg_rgb(  br_on, br_off,  br_on), # magenta
+    36:fg_rgb( br_off,  br_on,  br_on), # cyan
+    37:fg_rgb(  br_on,  br_on,  br_on), # white
+
+    40:bg_rgb( br_off, br_off, br_off), # black
+    41:bg_rgb(  br_on, br_off, br_off), # red
+    42:bg_rgb( br_off,  br_on, br_off), # green
+    43:bg_rgb(  br_on,  br_on, br_off), # yellow
+    44:bg_rgb( br_off, br_off,  br_on), # blue
+    45:bg_rgb(  br_on, br_off,  br_on), # magenta
+    46:bg_rgb( br_off,  br_on,  br_on), # cyan
+    47:bg_rgb(  br_on,  br_on,  br_on), # white
+}
+
+norm_on = 187
+norm_off = 0
+normtab = {
+    30:fg_rgb(norm_off, norm_off, norm_off), # black
+    31:fg_rgb( norm_on, norm_off, norm_off), # red
+    32:fg_rgb(norm_off,  norm_on, norm_off), # green
+    33:fg_rgb( norm_on,  norm_on, norm_off), # yellow
+    34:fg_rgb(norm_off, norm_off,  norm_on), # blue
+    35:fg_rgb( norm_on, norm_off,  norm_on), # magenta
+    36:fg_rgb(norm_off,  norm_on,  norm_on), # cyan
+    37:fg_rgb( norm_on,  norm_on,  norm_on), # white
+
+    40:bg_rgb(norm_off, norm_off, norm_off), # black
+    41:bg_rgb( norm_on, norm_off, norm_off), # red
+    42:bg_rgb(norm_off,  norm_on, norm_off), # green
+    43:bg_rgb( norm_on,  norm_on, norm_off), # yellow
+    44:bg_rgb(norm_off, norm_off,  norm_on), # blue
+    45:bg_rgb( norm_on, norm_off,  norm_on), # magenta
+    46:bg_rgb(norm_off,  norm_on,  norm_on), # cyan
+    47:bg_rgb( norm_on,  norm_on,  norm_on), # white
+}
 
 class AnsiEnscript(object):
 
@@ -35,28 +60,38 @@ class AnsiEnscript(object):
         ret = ''
 
         # Look for 'm' command
-        parse=re.split(r'\x1b\[([0-9]*);?([0-9]+)?m',line)
+        parse=re.split(r'\x1b\[([0-9;]*)([a-zA-Z])',line)
 
         while parse:
+            colortab = normtab
+
             ret += parse.pop(0)
             if not parse: break
-            intensity = parse.pop(0)
-            color = parse.pop(0)
 
-            if not intensity:
-                intensity = 0
-            else:
-                intensity = int(intensity)
+            args = parse.pop(0)
+            code = parse.pop(0)
 
-            if not color:
-                color = 0
-            else:
-                color = int(color)
+            if code=='m':
+                args = map(int,args.split(';'))
 
-            if (intensity,color) != (0,0):
-                ret += colortab[(intensity,color)]
-            else:
-                ret += '\0color{default}'
+                if not args:
+                    args = [0]
+
+                if args==[0]:
+                    ret += '\0color{default}'
+                    ret += '\0bgcolor{default}'
+
+                else:
+                    if 0 in args:
+                        colortab = normtab
+                    elif 1 in args:
+                        colortab = boldtab
+
+                    for arg in args:
+                        try:
+                            ret += colortab[arg]
+                        except KeyError:
+                            pass
 
         return ret
 
@@ -72,8 +107,6 @@ class AnsiEnscript(object):
 
     def __exit__(self, *args):
         pass
-
-import sys
 
 if __name__=="__main__":
     with AnsiEnscript(sys.stdout) as ae:
